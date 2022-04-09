caf2aff6519b9a232dbf20a0a1f92f7ec3d380ea
skyhaigu <skyhaigu@tencent.com>
2022-04-09 14:22:57 +0800

    integrate spark-sql with hive metastore

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ build() {
     IMAGE=bde2020/spark-$NAME:$TAG
     cd $([ -z "$2" ] && echo "./$NAME" || echo "$2")
     echo '--------------------------' building $IMAGE in $(pwd)
-    docker build -t $IMAGE .
+    docker build --no-cache -t $IMAGE .
     cd -
 }
 
@@ -17,6 +17,6 @@ build base
 build master
 build worker
 build submit
-build java-template template/java
-build scala-template template/scala
-build python-template template/python
+#build java-template template/java
+#build scala-template template/scala
+#build python-template template/python
